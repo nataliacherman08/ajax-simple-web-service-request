f8bae2b503ef77499548d1416412bd1191e35236
nataliacherman08 <cherman.natalia08@gmail.com>
2021-03-03 10:20:16 +0100

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,9 @@ const quote = document.getElementById("quote");
 const img = document.querySelector("img");
 const author = document.getElementById("author");
 
+let error = document.getElementById("error");
 
-fetch("https://thatsthespir.it/api")
+fetch("https://thatsthespir.it/api/2fhjehfk")
     .then(res => {
         if (res.ok) {
             res.json().then(data => {
@@ -11,5 +12,7 @@ fetch("https://thatsthespir.it/api")
                 quote.innerText = `" ${data.quote} "`;
                 author.innerText = data.author;
             })
+        } else {
+            error.innerHTML = "Something went wrong :("; // Not sure that it works
         }
     })
